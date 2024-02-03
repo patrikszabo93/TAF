6717c888fa72229fa79a5763eeaacc5f1834b3be
Patrik Szabó <patrikszabo93@gmail.com>
2024-02-03 16:37:52 +0100

    saucelabsonesie

--- TAF/src/main/java/swaglabs/NavigationTests.java
+++ TAF/src/main/java/swaglabs/NavigationTests.java
@@ -1,6 +1,9 @@
 package swaglabs;
 
 import common.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class NavigationTests extends CommonSwagLabsTestSteps {
@@ -11,5 +14,13 @@ public class NavigationTests extends CommonSwagLabsTestSteps {
        driver.get("https://www.saucedemo.com");
 
        loginWithUserAndPassword("standard_user","secret_sauce");
+
+       WebElement SauceLabsOnesieButton = driver.findElement(By.xpath("//*[@id=\"item_2_title_link\"]/div"));
+       SauceLabsOnesieButton.click();
+
+     //  Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/inventory-item.html?id=2");
+
+       boolean isWebpageContainsBadge = driver.getPageSource().contains("Sauce Labs Onesie");
+       Assert.assertTrue(isWebpageContainsBadge);
     }
 }
