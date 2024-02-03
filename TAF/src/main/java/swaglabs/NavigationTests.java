089b67c59cca8c11c4cf7bf583e7276b4fb0278c
Patrik Szabó <patrikszabo93@gmail.com>
2024-02-03 17:32:06 +0100

    pages

--- TAF/src/main/java/swaglabs/NavigationTests.java
+++ TAF/src/main/java/swaglabs/NavigationTests.java
@@ -9,45 +9,44 @@ import java.util.List;
 
 public class NavigationTests extends CommonSwagLabsTestSteps {
 
-    @Test
-    public void navigateToProductDetailsTest() {
-        //Előfeltétel
-        // oldal betöltése
-        driver.get("https://www.saucedemo.com");
+   @Test
+   public void navigateToProductDetailsTest() {
+      //Előfeltétel
+      // oldal betöltése
+      driver.get("https://www.saucedemo.com");
 
-        // 1. Bejelentkezés standard_user username-el és secret_sauce jelszóval
-        loginWithUserAndPassword("standard_user", "secret_sauce");
+      // 1. Bejelentkezés standard_user username-el és secret_sauce jelszóval
+      loginWithUserAndPassword("standard_user", "secret_sauce");
 
-        // 2. Sauce Labs Onesie termék címére rákattintás
+      // 2. Sauce Labs Onesie termék címére rákattintás
 
-        // WebElement SauceLabsOnesieButton = driver.findElement(By.xpath("//*[@id=\"item_2_title_link\"]/div"));
-        // WebElement SauceLabsOnesieButton = driver.findElement(By.xpath("//*[contains(text(),'Sauce Labs Onesie')]"));
-        List<WebElement> itemTitles = driver.findElements(By.className("inventory_item_name"));
-        itemTitles.get(4).click();
-        // SauceLabsOnesieButton.click();
+      // WebElement SauceLabsOnesieButton = driver.findElement(By.xpath("//*[@id=\"item_2_title_link\"]/div"));
+      // WebElement SauceLabsOnesieButton = driver.findElement(By.xpath("//*[contains(text(),'Sauce Labs Onesie')]"));
+      List<WebElement> itemTitles = driver.findElements(By.className("inventory_item_name"));
+      itemTitles.get(4).click();
+      // SauceLabsOnesieButton.click();
 
-        //Elvárt működés
-        // A termékrészletező oldalra navigáció sikeres, a termék neve "Sauce Labs Onesie"
-        boolean isWebpageContainsThisString = driver.getPageSource().contains("Sauce Labs Onesie");
-        Assert.assertTrue(isWebpageContainsThisString);
+      //Elvárt működés
+      // A termékrészletező oldalra navigáció sikeres, a termék neve "Sauce Labs Onesie"
+      boolean isWebpageContainsThisString = driver.getPageSource().contains("Sauce Labs Onesie");
+      Assert.assertTrue(isWebpageContainsThisString);
       // version1
-        WebElement productTitle = driver.findElement(By.className("inventory_details_name"));
-        Assert.assertEquals(productTitle.getText(),"Sauce Labs Onesie");
+      WebElement productTitle = driver.findElement(By.className("inventory_details_name"));
+      Assert.assertEquals(productTitle.getText(), "Sauce Labs Onesie");
       // version2
-       String actualUrl = driver.getCurrentUrl();
-       Assert.assertEquals(actualUrl,"https://www.saucedemo.com/inventory-item.html?id=2");
+      String actualUrl = driver.getCurrentUrl();
+      Assert.assertEquals(actualUrl, "https://www.saucedemo.com/inventory-item.html?id=2");
 
 
+      WebElement BacktoProductButton = driver.findElement(By.name("back-to-products"));
+      BacktoProductButton.click();
 
-        WebElement BacktoProductButton = driver.findElement(By.name("back-to-products"));
-        BacktoProductButton.click();
+      WebElement TestAllTheThingsButton = driver.findElement(By.xpath("//*[@id=\"item_3_title_link\"]/div"));
+      TestAllTheThingsButton.click();
 
-        WebElement TestAllTheThingsButton = driver.findElement(By.xpath("//*[@id=\"item_3_title_link\"]/div"));
-        TestAllTheThingsButton.click();
-
-        boolean isWebpageContainsThisString2 = driver.getPageSource().contains("Test.allTheThings() T-Shirt (Red)");
-        Assert.assertTrue(isWebpageContainsThisString2);
-    }
+      boolean isWebpageContainsThisString2 = driver.getPageSource().contains("Test.allTheThings() T-Shirt (Red)");
+      Assert.assertTrue(isWebpageContainsThisString2);
+   }
 
 
 }
---
+++ TAF/src/main/java/swaglabs/pages/LoginPage.java
@@ -0,0 +1,14 @@
+package swaglabs.pages;
+
+public class LoginPage {
+
+
+
+
+
+
+
+
+
+
+}
