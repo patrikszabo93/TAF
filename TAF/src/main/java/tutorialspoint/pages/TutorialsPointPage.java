6470f1301d7d5c3cec14b059feece6b749401d18
Patrik Szabó <patrikszabo93@gmail.com>
2024-02-08 17:35:34 +0100

    getsoutsall

--- TAF/src/main/java/tutorialspoint/pages/TutorialsPointPage.java
+++ TAF/src/main/java/tutorialspoint/pages/TutorialsPointPage.java
@@ -9,6 +9,7 @@ import java.io.File;
 import java.util.List;
 
 public class TutorialsPointPage {
+    private final String className = getClass().getSimpleName();
     private final WebDriver driver;
     private final By nameInputBy = By.id("name");
     private final By emailInputBy = By.id("email");
@@ -27,20 +28,24 @@ public class TutorialsPointPage {
     private final By loginButtonBy = By.cssSelector("input[value='Login']");
 
     public TutorialsPointPage(WebDriver driver) {
+        System.out.println("We assume that we're on the " + className);
         this.driver = driver;
     }
 
     public void fillNameInput(String text) {
+        System.out.println(className + ": We fill the name input field with : " + text);
         WebElement nameInput = driver.findElement(nameInputBy);
         nameInput.sendKeys(text);
     }
 
     public void fillEmailInput(String text) {
+        System.out.println(className + ": We fill the email input field with : " + text);
         WebElement emailInput = driver.findElement(emailInputBy);
         emailInput.sendKeys(text);
     }
 
     public void clickOnFemaleRadio() {
+        System.out.println(className + ": We click on the female radio button : ");
         List<WebElement> radioButtons = driver.findElements(radioButtonsBy);
         WebElement femaleRadio = radioButtons.get(1);  //2. radio button az oldalon
         femaleRadio.click();
@@ -51,43 +56,51 @@ public class TutorialsPointPage {
     }
 
     public void fillMobileInput(String text) {
+        System.out.println(className + ": We fill the mobile input field with : " + text);
         WebElement mobileInput = driver.findElement(mobileInputBy);
         mobileInput.sendKeys(text);
     }
 
     public void fillDateOfBirthInput(String text) {
+        System.out.println(className + ": We fill the birthday input field with : " + text);
         WebElement dateOfBirthInput = driver.findElement(dateOfBirthBy);
         dateOfBirthInput.sendKeys(text);
     }
 
     public void fillSubjectInput(String text) {
+        System.out.println(className + ": We fill the subject input field with : " + text);
         WebElement subjectInput = driver.findElement(subjectInputBy);
         subjectInput.sendKeys(text);
     }
 
     public void clickOnSportsCheckbox() {
+        System.out.println(className + ": We click on the sports checkboxes");
         List<WebElement> checkboxes = driver.findElements(checkboxesInputBy);
         WebElement sportsCheckbox = checkboxes.get(0);  //1. radio button az oldalon
         sportsCheckbox.click();
     }
 
     public void fillPictureFileUploadInput(String filename) {
+        System.out.println(className + ": We upload the following file : " +filename);
         String fileWithPath = new File(filename).getAbsolutePath();
         WebElement pictureInput = driver.findElement(pictureUploadInputByV2);
         pictureInput.sendKeys(fileWithPath);
     }
 
     public void fillCurrentAddressInput(String text) {
+        System.out.println(className + ": We fill the address input field with : " + text);
         WebElement subjectInput = driver.findElement(currentAddressInputBy);
         subjectInput.sendKeys(text);
     }
 
     public void selectState(String option) {
+        System.out.println(className + ": We select state "+ option);
         WebElement selectWebelement = driver.findElement(selectStateBy);
         Select select = new Select(selectWebelement);
         select.selectByVisibleText(option);
     }
     public void selectCity(String option) {
+        System.out.println(className + ":  We select state "+ option);
         WebElement selectWebelement = driver.findElement(selectCityBy);
         Select select = new Select(selectWebelement);
         select.selectByVisibleText(option);
@@ -96,12 +109,15 @@ public class TutorialsPointPage {
     }
 
     public void clickLoginButton(){
+        System.out.println(className + ": We click on login button");
         WebElement loginButton = driver.findElement(loginButtonBy);
         loginButton.click();
     }
     public boolean isLoginButtonClickable(){
         WebElement loginButton = driver.findElement(loginButtonBy);
-        return loginButton.isDisplayed()&& loginButton.isEnabled();
+        boolean isClickable =  loginButton.isDisplayed()&& loginButton.isEnabled();
+        System.out.println(className + ": We click on login button" + isClickable);
+        return isClickable;
     }
 
 }
